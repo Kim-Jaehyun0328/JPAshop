113073e1e2395b67ccf7c54e4a7c71e473e25822
KimJaehyun <rlawwogus@ajou.ac.kr>
2023-12-14 15:26:42 +0900

    proteced 기본 생성자 추가

--- jpashop/src/main/java/jpabook/jpashop/domain/Address.java
+++ jpashop/src/main/java/jpabook/jpashop/domain/Address.java
@@ -1,12 +1,23 @@
 package jpabook.jpashop.domain;
 
 import jakarta.persistence.Embeddable;
+import lombok.AccessLevel;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 @Embeddable //내장타입이라는 뜻
 @Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class Address {
     private String city;
     private String street;
     private String zipcode;
+
+    public Address(String city, String street, String zipcode) {
+        this.city = city;
+        this.street = street;
+        this.zipcode = zipcode;
+    }
+
 }
